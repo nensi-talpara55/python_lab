dcc645f872d061ebe45660884c9557bcb3e32ec5
nensi-talpara55 <talparanensi@gmail.com>
2026-01-31 08:06:07 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,9 @@
+#output using print()
+print("hello word")
+print("welcome to atmiya university")
+
+#input using input()
+name=input("enter your name:")
+print("hello",name)
+name=input("entyer your age:")
+
